5f9f50a933eb232b04abd7287728fc95775a3e1e
Juan Pablo Paternina <juan.paternina@gmail.com>
2025-01-21 08:10:04 +0000

    Excersise 1.3

--- part1/excersise/src/App.jsx
+++ part1/excersise/src/App.jsx
@@ -1,8 +1,8 @@
 const Part = (props) => {
   return (
-      <p>
-        {props.partName} {props.excersizeNumber}
-      </p>
+    <p>
+      {props.partName} {props.excersizeNumber}
+    </p>
   );
 };
 
@@ -15,7 +15,7 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
-  return ( 
+  return (
     <>
       <Part partName={props.partName} excersizeNumber={props.parNumber} />
       <Part partName={props.part2name} excersizeNumber={props.parte2number} />
@@ -35,28 +35,34 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
-      <Header course={course} partName={part1} excersizeNumber={exercises1} />
+      <Header course={course} partName={part1} excersizeNumber={course} />
       <Content
-      partName={part1}
-              parNumber={exercises1}
-        part2name={part2}
-        parte2number={exercises2}
-        part3name={part3}
-        parte3number={exercises3}
+        partName={part1.name}
+        parNumber={part1.exercises}
+        part2name={part2.name}
+        parte2number={part2.exercises}
+        part3name={part3.name}
+        parte3number={part3.exercises}
       />
       <Total
-        excersizeNumber1={exercises1}
-        excersizeNumber2={exercises2}
-        excersizeNumber3={exercises3}
+        excersizeNumber1={part1.exercises}
+        excersizeNumber2={part2.exercises}
+        excersizeNumber3={part3.exercises}
       />
     </div>
   );
